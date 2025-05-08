82458d0ddc15aa982b45b7d80c81e1e5259d993f
smit-patel-bacancy <smit.patel@bacancy.com>
2025-05-08 11:20:47 +0530

    UI Updated
    Update filter.pipe.ts
    
    Update app.component.html
    
    Update built-in-pipes-demo.component.html

--- src/app/pipes/filter.pipe.ts
+++ src/app/pipes/filter.pipe.ts
@@ -7,6 +7,9 @@ import { Pipe, PipeTransform } from '@angular/core';
 export class FilterPipe implements PipeTransform {
 
   transform(value: any, filterValue: string, propName: string): any {
+    // console.warn to check difference between pure and impure pipe
+    console.warn('value :', value, 'filterValue :', filterValue, 'propName :', propName);
+
     if (value.length == 0) {
       return value;
     }
